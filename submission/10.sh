0679cc161cd515f524229eaff4804dfe99d9e363
skodie0 <samuelmkodie@gmail.com>
2026-06-25 11:52:34 +0000

    fixing wrong 10 script

--- submission/10.sh
+++ submission/10.sh
@@ -1,3 +1,3 @@
 # Check if the following address is valid or invalid. Return only `true` or `false` - 
 # Address: `bcrt1qckgvfee4qs6y98jrcn8qc0m6ce6sxls0vac3yy` 
-true
+bitcoin-cli -regtest validateaddress "bcrt1qckgvfee4qs6y98jrcn8qc0m6ce6sxls0vac3yy" | jq -r '.isvalid'
