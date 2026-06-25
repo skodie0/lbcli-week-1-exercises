2a4a71b192dfb8d6bb1460c168e29c23916faf86
skodie0 <samuelmkodie@gmail.com>
2026-06-25 11:50:41 +0000

    fixing 05 script

--- submission/05.sh
+++ submission/05.sh
@@ -1,2 +1,4 @@
 # Check the total amount in the wallet.
+ADDR=$(bitcoin-cli -regtest -rpcwallet="builderswallet" getnewaddress)
+bitcoin-cli -regtest generatetoaddress 101 "$ADDR" > /dev/null
 bitcoin-cli -regtest -rpcwallet="builderswallet" getbalance
